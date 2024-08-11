c0428e6d6846f1cbb525afafe5370285e94d4705
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-08-11 10:03:22 +0200

    sorted assets folder and added player movement + early rotations

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float MoveSpeed = 15f;
 
-    // Update is called once per frame
+    [SerializeField] float xRange = 9f;
+    [SerializeField] float yRange = 5f;
+
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
-        float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);
+        ProccessTranslation();
+        ProccessRotation();
+    }
+
+    void ProccessRotation()
+    {
+        transform.localRotation = Quaternion.Euler(-30f, 30f, 0f);
+    }
+
+    private void ProccessTranslation()
+    {
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * MoveSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float newXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yOffset = yThrow * Time.deltaTime * MoveSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float newYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
     }
 }
