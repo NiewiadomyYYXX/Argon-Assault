e587bf44b5a7cfefa9162d0e74339b4cc37226ba
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-08-14 10:15:57 +0200

    reload after death + fix of particles

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    bool isAlive = true;
+    [SerializeField] float levelLoadDelay = 1f;
+
     void OnTriggerEnter(Collider other)
     {
         Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");   
+        if ( isAlive == true)
+        {
+            StartCrash();
+        }
+    }
+
+    void StartCrash()
+    {
+        isAlive = false;
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
