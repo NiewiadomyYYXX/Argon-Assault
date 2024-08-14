51aa60609566da88c139e7923b57ebbc653774c1
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-08-14 08:41:12 +0200

    tooltips and headers fixing code variables

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    [SerializeField] float MoveSpeed = 30f;
+    [Header("General Setup Settings")]
+    [Tooltip("How fast ship moves based upon player input")] [SerializeField] float MoveSpeed = 30f;
+    [Tooltip("Max range of player movement left and right")] [SerializeField] float xRange = 10f;
+    [Tooltip("Max range of player movement up and down")] [SerializeField] float yRange = 9f;
 
-    [SerializeField] float xRange = 10f;
-    [SerializeField] float yRange = 9f;
-
-    [SerializeField] float pitchFactor = -2f;
-    [SerializeField] float controlPitchFactor = -10f;
-    [SerializeField] float controlYawFactor = 5f;
-    [SerializeField] float controlRollFactor = -15f;
-
+    [Header("Laser gun array")]
+    [Tooltip("Add all player lasers here")]
     [SerializeField] GameObject[] lasers;
+
+    [Header("Screen posistion based tuning")]
+    [Tooltip("idk moving something")] [SerializeField] float pitchFactor = -2f;
+    [Tooltip("idk moving something")] [SerializeField] float controlYawFactor = 5f;
+
+    [Header("Screen posistion based tuning")]
+    [Tooltip("idk moving something")] [SerializeField] float controlPitchFactor = -10f;
+    [Tooltip("idk moving something")] [SerializeField] float controlRollFactor = -15f;
+
 
     float xThrow, yThrow;
 
