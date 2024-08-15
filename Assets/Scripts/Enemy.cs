ba646a962c7b0f425af6ec7651c14443836f337e
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-08-15 08:38:14 +0200

    ui - create

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,30 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [Tooltip("How much score will player get after killing that enemy")][SerializeField] int score;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindAnyObjectByType<ScoreBoard>();    
+    }
 
     void OnParticleCollision(GameObject other)
+    {
+        ProccessHit();
+        KillEnemy();
+    }
+
+    void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
+    }
+
+    void ProccessHit()
+    {
+        scoreBoard.IncreaseScore(score);
     }
 }
