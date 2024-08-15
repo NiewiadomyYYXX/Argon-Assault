294c353c80a239c9762752aa8d958c0e540025b2
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-08-15 07:56:48 +0200

    enemy particles

---
+++ Assets/Scripts/SelfDestroy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    [SerializeField] float timeTillDestoy = 3f;
+    void Start()
+    {
+        Destroy(gameObject, timeTillDestoy);
+    }
+}
